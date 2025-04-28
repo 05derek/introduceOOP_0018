7b664022e074e454528b6441048ff7824562e281
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-04-28 11:15:28 +0700

    output data class mobil

--- classMobil.cpp
+++ classMobil.cpp
@@ -1,10 +1,6 @@
 
-        public:
-        void input_Data(){
-            cout << "masukkan nama mobil= ";
-            cin >> nama_Mobil;
-            cout << "masukkan tipe mobil= ";
-            cin >> tipe_Mobil;
-            cout << "masukkan harga mobil= ";
-            cin >> harga_Mobil;
- 
+        }
+        void output_data(){
+            cout << "harga mobil= " << harga_Mobil << endl; 
+        }
+    
