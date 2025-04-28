0fd220b41176280d02cc5ecfb7775207a82838bb
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-04-28 11:14:27 +0700

    input data class mobil

---
+++ classMobil.cpp
@@ -0,0 +1,10 @@
+
+        public:
+        void input_Data(){
+            cout << "masukkan nama mobil= ";
+            cin >> nama_Mobil;
+            cout << "masukkan tipe mobil= ";
+            cin >> tipe_Mobil;
+            cout << "masukkan harga mobil= ";
+            cin >> harga_Mobil;
+ 
