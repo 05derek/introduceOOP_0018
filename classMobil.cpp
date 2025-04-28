d6285b94a71f423052e6534571891454da90ebd7
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-04-28 11:15:59 +0700

    int main

--- classMobil.cpp
+++ classMobil.cpp
@@ -1,6 +1,12 @@
 
-        }
-        void output_data(){
-            cout << "harga mobil= " << harga_Mobil << endl; 
-        }
     
+    
+    int main(){
+        mobil derek;
+        derek.input_Data();
+    
+        cout << "nama mobil= " << derek.nama_Mobil << endl;
+        cout << "tipe mobil= " << derek.tipe_Mobil << endl;
+        derek.output_data();
+    
+    }  
